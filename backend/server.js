a68782b8dd54c0b4d2806a990159f7fddde835f7
dps369369 <debpriosarker11@gmail.com>
2026-07-21 05:10:29 +0600

    Create V0.2 full stack structure

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 369;
+
+
+app.get("/", function (req, res) {
+
+    res.send("Aegis Backend is Running");
+
+});
+
+
+app.listen(PORT, function () {
+
+    console.log(`Aegis server running on port ${PORT}`);
+
+});
